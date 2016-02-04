71445508527559fead2b2e899978b09a85349a76
Edgar Rivero <edgar@vauxoo.com>
2016-02-04 18:16:52 +0000

    [REF] openacademy: Add partner vire inherit and model inherit

---
+++ openacademy/model/partner.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    instructor = fields.Boolean(default=False)
+    session_ids = fields.Many2many('openacademy.session', 
+        string='Session as instructor', readonly=True)
